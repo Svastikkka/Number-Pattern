60c830e4124b591dfcac92b1139cb5c8526f4134
Manshu Sharma <36654754+Svastikkka@users.noreply.github.com>
2020-04-30 04:46:38 +0530

    Star/ Diamond Pattern
    7  Number is always odd
       *
      ***
     *****
    *******
     *****
      ***
       *

---
+++ Code : Star Pattern.py
@@ -0,0 +1,44 @@
+"""
+
+7  Number is always odd
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+"""
+
+
+n=int(input())
+firstHalf = (n+1)//2
+secondHalf=n//2
+
+currentRow=1
+while currentRow<=firstHalf:
+    spaces=1
+    while spaces<=(firstHalf-currentRow):
+        print(" ",end="")
+        spaces=spaces+1
+
+    currentCol=1
+    while currentCol<=(2*currentRow)-1:
+        print("*",end="")
+        currentCol=currentCol+1
+
+    print()
+    currentRow+=1
+
+currentRow=secondHalf
+while currentRow>=1:
+    spaces=1
+    while spaces <= (secondHalf-currentRow+1):
+        print(" ",end="")
+        spaces=spaces+1
+    currentCol=1
+    while currentCol<=(2*currentRow)-1:
+        print("*",end="")
+        currentCol+=1
+    print()
+    currentRow-=1
